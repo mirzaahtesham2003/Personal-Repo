c05605888b38926ee62b9175c6d6c229d0a55d03
mirzaahtesham2003 <mirzaahtesham2003@gmail.com>
2024-10-17 18:57:05 +0530

    New project

---
+++ Angular/Project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './user-access/login/login.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Project';
+}
---
+++ Angular/Project/src/app/user-access/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+    username: string = '';
+    password: string = '';
+
+    login(){
+      alert(`Hi ${this.username} this project is under development.`);
+    }
+
+}
